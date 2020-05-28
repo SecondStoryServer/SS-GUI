14e978481816cd57518decac4a08885bf5282473
sya-ri <sya79lua@gmail.com>
2020-05-28 13:04:32 +0900

    アイテムがないときにファイルを削除するように変更

--- src/main/kotlin/me/syari/ss/gui/vanilla/VanillaInventoryPlayer.kt
+++ src/main/kotlin/me/syari/ss/gui/vanilla/VanillaInventoryPlayer.kt
@@ -1,7 +1,8 @@
 package me.syari.ss.gui.vanilla
 
 import me.syari.ss.core.Main.Companion.console
-import me.syari.ss.core.config.CreateConfig
+import me.syari.ss.core.config.CreateConfig.config
+import me.syari.ss.core.config.CustomConfig
 import me.syari.ss.core.config.dataType.ConfigDataType
 import me.syari.ss.core.item.InventoryBase64
 import me.syari.ss.core.player.UUIDPlayer
@@ -10,12 +11,20 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.PlayerInventory
 
 data class VanillaInventoryPlayer(val uuidPlayer: UUIDPlayer) {
-    private val config = CreateConfig.config(guiPlugin, console, "VanillaInventory/${uuidPlayer}.yml")
+    private var nullableConfig: CustomConfig? = null
     private var contentsCache: Array<ItemStack?>? = null
+
+    private fun getConfigOrCreate(): CustomConfig {
+        return nullableConfig ?: {
+            config(guiPlugin, console, "VanillaInventory/${uuidPlayer}.yml").apply {
+                nullableConfig = this
+            }
+        }.invoke()
+    }
 
     fun load(): Array<ItemStack?>? {
         return if (contentsCache == null) {
-            val base64 = config.get("base64", ConfigDataType.STRING, false) ?: return null
+            val base64 = getConfigOrCreate().get("base64", ConfigDataType.STRING, false) ?: return null
             val inventory = InventoryBase64.getInventoryFromBase64(base64)
             inventory.contents.apply {
                 contentsCache = this
@@ -26,8 +35,16 @@ data class VanillaInventoryPlayer(val uuidPlayer: UUIDPlayer) {
     }
 
     fun save(playerInventory: PlayerInventory) {
-        val base64 = InventoryBase64.toBase64(playerInventory)
-        config.set("base64", base64, true)
-        contentsCache = playerInventory.contents
+        val contents = playerInventory.contents
+        if (contents.filterNotNull().isNotEmpty()) {
+            val base64 = InventoryBase64.toBase64(contents)
+            getConfigOrCreate().set("base64", base64, true)
+        } else {
+            nullableConfig?.let {
+                it.delete()
+                nullableConfig = null
+            }
+        }
+        contentsCache = contents
     }
 }
